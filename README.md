4f8a0fd543e63a22d86c8f60e5e62cf682e34e53
Michael Pouris <mpouris@users.noreply.github.com>
2014-06-04 22:27:02 -0400

    Added first migration to begin creating an authentication system. This includes created a DB in MYSQL (I have listed the command in README.MD), and setting up the DB credentials in ap/config/database.php.
    In addition, I added a .gitignore file to remove any files that are
    not needed for this project. More specifically, anything in the vendors
    folder is not needed as this should be handled by each developer's
    composer enviroment.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # LearnBook
 
 A social networking app created with Laravel. Its purpose is to enable us to learn more about the PHP MVC framework.
+
+The team will be adding in some useful links that they used to become familiar with Laravel and GIT.
+
+# 1. Getting GIT to ignore specific files:
+    http://git-scm.com/docs/gitignore
+
+# 2. Creating a database for Eloquent:
+    CREATE DATABASE `LearnBook` DEFAULT CHARACTER SET='utf8' DEFAULT COLLATE='utf8_unicode_ci';
+
+    After the DB is created, enter the correct information in database.php
+
+# 3. It is time to make a users table for authentication:
+    http://laravelbook.com/laravel-migrations-managing-databases/
+    http://laravel.com/docs/schema
+
+    The command to create a basic users table is:
+        php artisan migrate:make create_users_table --table users --create
+
+
---
+++ app/database/migrations/2014_06_05_015906_create_users_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        //Note that the users table is defined as the authentication
+        //table set in auth.php. The users table maps to User in the model
+		Schema::table('users', function(Blueprint $table)
+		{
+			//Create a Primary Indexed Key:
+            $table->increments('id');
+            
+            //Simply create strings for firstname
+            //and the last name with a limit
+            $table->string('firstName',50);
+            $table->string('lastName',50);
+
+            //This will be used to specify the password
+            //and logins for the application. Unique
+            //is automatically indexed
+            $table->string('email')->unique();
+            $table->string('password');
+
+            //Need this to have remembering logged in user
+            $table->string('remember_token',100)->nullable();
+
+            //Specify that we want to have soft deletes
+            //That is we will never delete data only
+            //apply a timestamp when the data is 'deleted'
+            $table->softDeletes();
+
+            //We want to track the created_at and updated_at
+            //this is done with one method call. Remember,
+            //this timestamp not equivalent to a MYSQL timestamp
+            $table->timestamp();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
