cba1fe5005ee7436b91ff63d25c60525af331366
Michael Pouris <mpouris@users.noreply.github.com>
2014-06-05 22:54:11 -0400

    Added in soft deletes for the Users Table. This should be added into every model.

--- app/models/User.php
+++ app/models/User.php
@@ -5,6 +5,17 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
+    protected $softDelete = true;
+
+    //Validation rules for registering
+    public static $rules = array(
+        'firstname'=>'required|alpha|min:2',
+        'lastname'=>'required|alpha|min:2',
+        'email'=>'required|email|unique:users',
+        'password'=>'required|alpha_num|between:6,12|confirmed',
+        'password_confirmation'=>'required|alpha_num|between:6,12'
+    );
+
 	/**
 	 * The database table used by the model.
 	 *
